b04bad93026df65911a4c6cbf164d03c4f784ac5
nyangshawbin <nyangbin@gmail.com>
2023-12-01 12:24:05 +0800

    feat: add comments

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,3 +1,6 @@
+/**
+ * to consider moving into standlone node server for scalability/ microservice architecture
+ */
 import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import OpenAI from 'openai'
@@ -13,8 +16,10 @@ const openai = new OpenAI({
 
 export async function POST(req: Request) {
   const json = await req.json()
-  const { messages, previewToken } = json
+  const { messages, previewToken, id: chatID } = json
   const userId = (await auth())?.user.id
+  console.log('🚀 chatID:', chatID)
+  console.log('🚀 userId:', userId)
 
   if (!userId) {
     return new Response('Unauthorized', {
@@ -22,20 +27,21 @@ export async function POST(req: Request) {
     })
   }
 
+  // only for preview env
   if (previewToken) {
     openai.apiKey = previewToken
   }
 
   const res = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
-    messages,
+    messages, // incoming + chat history
     temperature: 0.7,
     stream: true
   })
 
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
+      const title = json.messages[0].content.substring(0, 100) // first 100 char as chat title
       const id = json.id ?? nanoid()
       const createdAt = Date.now()
       const path = `/chat/${id}`
